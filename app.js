930ba01952619555dd6fa61c9beebd609b6bcc16
Fowder <emile.troccaz@hotmail.com>
2017-12-14 15:22:34 +0100

    ajout travail

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // good luck !
+
+function main() {
+	$('.color').each(function(){
+		var color = $(this).data('color');
+		$(this).attr('style', 'background-color:'+color+'');
+	});
+
+$('.color').click(function(){
+	var check = $('#modify-texte').is(':checked');
+	var color = $(this).data('color');
+
+		if (check){
+		$('.main').css('color', color);
+
+		}else{
+
+		$('.main').css('background-color', color);
+	}
+});
+}
+
+$(document).ready(function(){
+	main();
+});
